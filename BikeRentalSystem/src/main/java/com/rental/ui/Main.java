56f848c65384551a136e9b3b9ebc7c3fc9f31788
Cl1ngz <ByteIgma@gmail.com>
2025-04-10 15:17:54 +0200

    functions to implement in future

--- BikeRentalSystem/src/main/java/com/rental/ui/Main.java
+++ BikeRentalSystem/src/main/java/com/rental/ui/Main.java
@@ -7,6 +7,16 @@ public class Main {
     }
 
     private static void initializeSystem() {
-        // TODO:
+        // TODO: initialize system
+    }
+    private static void printMenu() {
+        // TODO: print menu
+    }
+    private static int getUserChoice() {
+        // TODO: get user choice
+        return -100;
+    }
+    private static void registerUser() {
+        // TODO: register user
     }
 }
