c9ccb3968981fc785f3addb3de906d4dfddd0755
Cl1ngz <ByteIgma@gmail.com>
2025-05-09 23:24:02 +0200

    Added User Managment in BikeRentalSystem

--- BikeRentalSystem/src/main/java/com/rental/service/BikeRentalSystem.java
+++ BikeRentalSystem/src/main/java/com/rental/service/BikeRentalSystem.java
@@ -1,5 +1,43 @@
 package com.rental.service;
 
+import com.rental.exception.UserNotFoundException;
+import com.rental.model.Bike;
+import com.rental.model.Station;
+import com.rental.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class BikeRentalSystem {
-    // TODO: BikeRentalSystem
+    private Map<String, User> users = new HashMap<>();
+    private Map<String, Station> stations = new HashMap<>();
+    private Map<String, Bike> bikes = new HashMap<>();
+
+    // --- User Management ---
+    public User registerUser(String userId, String name) {
+        if (users.containsKey(userId)) {
+            throw new IllegalArgumentException("Użytkownik o ID " + userId + " już istnieje.");
+        }
+        User newUser = new User(userId, name);
+        users.put(userId, newUser);
+        System.out.println("Zarejestrowano użytkownika: " + newUser);
+        return newUser;
+    }
+
+    public User findUser(String userId) throws UserNotFoundException {
+        User user = users.get(userId);
+        if (user == null) {
+            throw new UserNotFoundException("Nie znaleziono użytkownika o ID: " + userId);
+        }
+        return user;
+    }
+
+    // --- Station Management ---
+    // TODO: Implement methods for managing stations
+
+    // --- Bike Management ---
+    // TODO: Implement methods for managing bikes
+
+    // --- Rental Management ---
+    // TODO: Implement methods for managing rentals
 }
